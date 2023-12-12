4765f91fd160ac9783e95a12cbdb98062bd0201c
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2023-12-12 17:57:58 +0100

    configured and registered create, mint

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Psalmuel-Chain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Psalmuel"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PSM"
 )
 
 var ID ids.ID
